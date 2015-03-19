83d0816c19cb19950e89d0f6b6bedc5ddc928898
Andrew Clinton <ddrew.clinton@gmail.com>
2015-03-19 14:35:19 -0400

    added a factory to manage rotten tomatoes http requests and another to help build query strings

--- js/app/factories.js
+++ js/app/factories.js
@@ -1,5 +1,49 @@
 angular.module('app.factories', [])
 
-.factory('AuthFactory', [function() {
-    return {}
+.factory('RottenTomatoesFactory', ['RottenTomatoesConfig', 'QueryStringBuilderFactory', 'HttpService', function(RTC, QSBF, HttpService) {
+    /* PRIVATE VARS */
+    var _q, _pageLimit = 16,
+        _page = 1,
+        _country = 'us',
+        _apiKey = RTC.key;
+
+    /* PUBLIC METHODS */
+    return {
+    	//MOVIE LISTS
+        inTheaters: function() {
+            var url = RTC.base_url + '/lists/movies/in_theaters.json';
+            url += QSBF.buildQuery(['page_limit', 'page', 'country', 'apikey'], [_pageLimit, _page, _country, _apiKey]);
+            return HttpService.get(url);
+        },
+        openingMovies: function() {
+            var url = RTC.base_url + '/lists/movies/opening.json';
+            url += QSBF.buildQuery(['page_limit', 'page', 'country', 'apikey'], [_pageLimit, _page, _country, _apiKey]);
+            return HttpService.get(url);
+        },
+        upcomingMovies: function() {
+            var url = RTC.base_url + '/lists/movies/upcoming.json';
+            url += QSBF.buildQuery(['page_limit', 'page', 'country', 'apikey'], [_pageLimit, _page, _country, _apiKey]);
+            return HttpService.get(url);
+        }
+    }
+}])
+
+/**
+*	This factory builds a query string from {@code keys} and {@code values}
+* 	and returns the built query string
+*/
+.factory('QueryStringBuilderFactory', [function() {
+    return {
+        buildQuery: function(keys, values) {
+            if (keys.length != values.length) {
+                return false;
+            }
+            var i = 0,
+                string = "?";
+            for (i; i < keys.length; i++) {
+                string += "&" + keys[i] + '=' + values[i];
+            }
+            return string;
+        }
+    }
 }]);
