6b033789acf2b5687df733d1b7128f99cacb05c6
Andrew Clinton <ddrew.clinton@gmail.com>
2015-03-19 10:11:59 -0400

    stripped out this branch to not include any google calendar or oauth api stuffs

--- js/app/app.js
+++ js/app/app.js
@@ -1,4 +1,4 @@
-angular.module('app', ['ui.router', 'oauth', 'ngStorage', 'ngResource', 'ngSanitize', 'app.controllers', 'app.factories', 'app.services'])
+angular.module('app', ['ui.router', 'ngStorage', 'ngResource', 'ngSanitize', 'app.controllers', 'app.factories', 'app.services'])
     .run(['$rootScope', '$state', '$stateParams', '$location', 'AuthFactory',
         function($rootScope, $state, $stateParams, $location) {
         }
@@ -7,33 +7,19 @@ angular.module('app', ['ui.router', 'oauth', 'ngStorage', 'ngResource', 'ngSanit
 .config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {
     $stateProvider
 
+    //root controller for whole app
     .state('site', {
         abstract: true,
         controller: 'ApplicationCtrl',
         templateUrl: 'templates/site.html'
     })
 
+    //controller for just the "main.html" view
     .state('site.main', {
         url: '/main',
         controller: 'MainCtrl',
         templateUrl: 'templates/main.html'
     })
 
-    .state('site.logging', {
-        url: '/access_token=:accessToken',
-        template: "woahhhh here?",
-        controller: function($location, AccessToken) {
-            var hash = $location.path().substr(1);
-            AccessToken.setTokenFromString(hash);
-        }
-    })
-
-    .state('site.login', {
-        url: '/login',
-        templateUrl: 'templates/login.html',
-        controller: 'LogInCtrl'
-    })
-
-    $urlRouterProvider.otherwise('/login');
-    //$locationProvider.html5Mode(true).hashPrefix('!');
+    $urlRouterProvider.otherwise('/main');
 }]);
