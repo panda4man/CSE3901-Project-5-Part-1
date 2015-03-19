1997faf0381a4b1a754a63fc134c1321c81d905a
Andrew Clinton <ddrew.clinton@gmail.com>
2015-03-19 14:34:51 -0400

    added a service to wrap up the http and q angular services and help abstract logic into multiple layers

--- js/app/services.js
+++ js/app/services.js
@@ -1,4 +1,25 @@
 angular.module('app.services', [])
 
-.service('SessionService', function() {
-});
+/**
+*	This is a custom http service to fill the needs of the two apis
+*	used in this project. If we were using more RESTful API's
+* 	we would have gone with angular's $resource instead.
+*/
+.service('HttpService', ['$http', '$q', function($http, $q) {
+    return {
+        get: function(url) {
+            var deferred = $q.defer();
+            $http.get(url).success(function(data, status, headers, config) {
+                deferred.resolve(data);
+            }).error(function(data, status, headers, config) {
+                var object = {
+                    data: data,
+                    status: status,
+                    config: config
+                }
+                deferred.reject(object);
+            });
+            return deferred.promise;
+        }
+    }
+}]);
