fa8a9813caecdab97fa52c9eeac79ef88e403394
Andrew Clinton <ddrew.clinton@gmail.com>
2015-03-20 10:19:16 -0400

    added new controllers

--- js/app/controllers.js
+++ js/app/controllers.js
@@ -1,17 +1,25 @@
 angular.module('app.controllers', [])
 
-.controller('ApplicationCtrl', function($scope, $rootScope, $timeout, $location) {
-    
+.controller('ApplicationCtrl', function($scope, Environment) {
+    $scope.debug = Environment.debug;
 })
 
-.controller('MainCtrl', function($scope, $http, RottenTomatoesFactory) {
-    $scope.title = "heyy i am a title :)";
-
-    $scope.test = function () {
-        RottenTomatoesFactory.upcomingMovies().then(function(data){
-            console.log(data);
-        }, function (data){
-            console.log(data);
+.controller('MainCtrl', function($scope, $http, RottenTomatoesFactory, Environment, Console, $localStorage) {
+    $scope.test = function() {
+        RottenTomatoesFactory.boxOffice().then(function(data) {
+            Console.log(data);
+        }, function(data) {
+            Console.log(data);
         });
     }
+
+    $scope.movies = $localStorage.movies;
+})
+
+.controller('LandingCtrl', function ($scope){
+
+})
+
+.controller('AboutCtrl', function ($scope){
+
 });
