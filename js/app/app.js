72141977ecd33185d9a01ecb0dfac6fc53f20083
Andrew Clinton <ddrew.clinton@gmail.com>
2015-03-20 10:19:06 -0400

    preload all json data from rotten tomatoes if need be and new routes

--- js/app/app.js
+++ js/app/app.js
@@ -1,7 +1,58 @@
 angular.module('app', ['ui.router', 'ngStorage', 'ngResource', 'ngSanitize', 'app.controllers', 'app.factories', 'app.services'])
-    .run(['$rootScope', '$state', '$stateParams', '$location',
-        function($rootScope, $state, $stateParams, $location) {}
-    ])
+    .run(['RottenTomatoesFactory', 'Console', 'MovieStorageFactory', 'DvdStorageFactory', function(RottenTomatoesFactory, Console, MSF, DSF) {
+        //see if we need to update local movie data
+        if (MSF.needToWriteStorage()) {
+            RottenTomatoesFactory.boxOffice().then(function(data) {
+                MSF.writeBoxOffice(data);
+                Console.log('Got the box office data');
+            }, function(data) {
+                Console.log(data);
+            });
+            RottenTomatoesFactory.inTheaters().then(function(data) {
+                MSF.writeInTheaters(data);
+                Console.log('Got the in theater data');
+            }, function(data) {
+                Console.log(data);
+            });
+            RottenTomatoesFactory.openingMovies().then(function(data) {
+                MSF.writeOpeningMovies(data);
+                Console.log('Got the opening movies data');
+            }, function(data) {
+                Console.log(data);
+            });
+            RottenTomatoesFactory.upcomingMovies().then(function(data) {
+                MSF.writeUpcomingMovies(data);
+                Console.log('Got the upcoming movies data');
+            }, function(data) {
+                Console.log(data);
+            });
+        } else {
+            Console.log("Do not need to update movies.");
+        }
+        //see if we need to update local dvd data
+        if(DSF.needToWriteStorage()){
+            RottenTomatoesFactory.currentRelease().then(function(data) {
+                DSF.writeCurrentRelease(data);
+                Console.log('Got the current release dvd data.');
+            }, function(data) {
+                Console.log(data);
+            });
+            RottenTomatoesFactory.newRelease().then(function(data) {
+                DSF.writeNewRelease(data);
+                Console.log('Got the new release dvd data.');
+            }, function(data) {
+                Console.log(data);
+            });
+            RottenTomatoesFactory.upcomingRelease().then(function(data) {
+                DSF.writeUpcomingRelease(data);
+                Console.log('Got the upcoming release dvd data.');
+            }, function(data) {
+                Console.log(data);
+            });
+        } else {
+            Console.log('Do not need to update dvds.');
+        }
+    }])
 
 .config(['$stateProvider', '$urlRouterProvider', '$locationProvider', '$httpProvider', function($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
     $stateProvider
@@ -20,10 +71,28 @@ angular.module('app', ['ui.router', 'ngStorage', 'ngResource', 'ngSanitize', 'ap
         templateUrl: 'templates/main.html'
     })
 
-    $urlRouterProvider.otherwise('/main');
+    .state('site.landing', {
+        url: '/',
+        controller: 'LandingCtrl',
+        templateUrl: 'templates/landing.html'
+    })
+
+    .state('site.about', {
+        url: '/about',
+        controller: "AboutCtrl",
+        templateUrl: 'templates/about.html'
+    })
+
+    $urlRouterProvider.otherwise('/');
+    $locationProvider.html5Mode(true);
 }])
 
 .constant('RottenTomatoesConfig', {
     'key': 'tpuzgqpjcjtvszmgvf9fqjfg',
     'base_url': 'http://api.rottentomatoes.com/api/public/v1.0'
+})
+
+.constant('Environment', {
+    'debug': true,
+    'isDevelopment': true
 });
