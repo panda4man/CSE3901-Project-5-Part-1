3c19fa8218af472634443d1c377985e3cda88af1
Andrew Clinton <ddrew.clinton@gmail.com>
2015-03-19 22:29:07 -0400

    small changes

--- js/dependencies/custom.js
+++ js/dependencies/custom.js
@@ -11,6 +11,6 @@
         $(document).ready(function() {
             $('select').material_select();
         });
-        
+
     }); // end of document ready
 })(jQuery); // end of jQuery name space
