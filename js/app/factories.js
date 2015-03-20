aa81240cf7a795df20f35ed11f92d332cc946ff7
Andrew Clinton <ddrew.clinton@gmail.com>
2015-03-20 10:19:31 -0400

    bunch of handy new factories

--- js/app/factories.js
+++ js/app/factories.js
@@ -10,6 +10,11 @@ angular.module('app.factories', [])
     /* PUBLIC METHODS */
     return {
     	//MOVIE LISTS
+    	boxOffice: function () {
+    		var url = RTC.base_url + '/lists/movies/box_office.json';
+            url += QSBF.buildQuery(['page_limit', 'page', 'country', 'apikey'], [_pageLimit, _page, _country, _apiKey]);
+            return HttpService.get(url);
+    	},
         inTheaters: function() {
             var url = RTC.base_url + '/lists/movies/in_theaters.json';
             url += QSBF.buildQuery(['page_limit', 'page', 'country', 'apikey'], [_pageLimit, _page, _country, _apiKey]);
@@ -24,8 +29,101 @@ angular.module('app.factories', [])
             var url = RTC.base_url + '/lists/movies/upcoming.json';
             url += QSBF.buildQuery(['page_limit', 'page', 'country', 'apikey'], [_pageLimit, _page, _country, _apiKey]);
             return HttpService.get(url);
+        },
+        search: function (query) {
+        	var url = RTC.base_url + '/lists/movies';
+        	url += QSBF.buildQuery(['q', 'page_limit', 'page', 'country', 'apikey'], [encodeURI(query), _pageLimit, _page, _country, _apiKey]);
+        	return HttpService.get(url);
+        },
+        //DVD LISTS
+        currentRelease: function () {
+        	var url = RTC.base_url + '/lists/dvds/current_releases.json';
+        	url += QSBF.buildQuery(['page_limit', 'page', 'country', 'apikey'], [_pageLimit, _page, _country, _apiKey]);
+        	return HttpService.get(url);
+        },
+        newRelease: function () {
+        	var url = RTC.base_url + '/lists/dvds/new_releases.json';
+        	url += QSBF.buildQuery(['page_limit', 'page', 'country', 'apikey'], [_pageLimit, _page, _country, _apiKey]);
+        	return HttpService.get(url);
+        },
+        upcomingRelease: function () {
+        	var url = RTC.base_url + '/lists/dvds/upcoming.json';
+        	url += QSBF.buildQuery(['page_limit', 'page', 'country', 'apikey'], [_pageLimit, _page, _country, _apiKey]);
+        	return HttpService.get(url);
         }
     }
+}])
+
+/**
+*	This factory provides method for writing the movie rottentomatoes data chunks to localstorage.
+*	In this way we prevent the user fromg needing to refresh data via http calls.
+*/
+.factory('MovieStorageFactory', ['$localStorage', function ($localStorage) {
+	var _oneDay = 24*60*60*1000;
+	if(!$localStorage.movies){
+		$localStorage.movies = {}
+	}
+	return {
+		writeBoxOffice: function (data) {
+			$localStorage.movies.boxOffice = data;
+			$localStorage.movies.lastWrote = new Date();
+		},
+		writeInTheaters: function (data) {
+			$localStorage.movies.inTheaters = data;
+			$localStorage.movies.lastWrote = new Date();
+		},
+		writeOpeningMovies: function (data) {
+			$localStorage.movies.openingMovies = data;
+			$localStorage.movies.lastWrote = new Date();
+		},
+		writeUpcomingMovies: function (data) {
+			$localStorage.movies.upcomingMovies = data;
+			$localStorage.movies.lastWrote = new Date();
+		},
+		needToWriteStorage: function () {
+			var now = new Date();
+			if ($localStorage.movies.lastWrote == null) {
+				return true;
+			}
+			var lastWrote = new Date($localStorage.movies.lastWrote);
+			var days = Math.round(Math.abs((now.getTime() - lastWrote.getTime())/(_oneDay)));
+			return (days > 1);
+		}
+	}
+}])
+
+/**
+*	This factory provides method for writing the dvd rottentomatoes data chunks to localstorage.
+*	In this way we prevent the user fromg needing to refresh data via http calls.
+*/
+.factory('DvdStorageFactory', ['$localStorage', function ($localStorage) {
+	var _oneDay = 24*60*60*1000;
+	if(!$localStorage.dvds){
+		$localStorage.dvds = {}
+	}
+	return {
+		writeCurrentRelease: function (data) {
+			$localStorage.dvds.inTheaters = data;
+			$localStorage.dvds.lastWrote = new Date();
+		},
+		writeUpcomingRelease: function (data) {
+			$localStorage.dvds.openingMovies = data;
+			$localStorage.dvds.lastWrote = new Date();
+		},
+		writeNewRelease: function (data) {
+			$localStorage.dvds.upcomingMovies = data;
+			$localStorage.dvds.lastWrote = new Date();
+		},
+		needToWriteStorage: function () {
+			var now = new Date();
+			if ($localStorage.dvds.lastWrote == null) {
+				return true;
+			}
+			var lastWrote = new Date($localStorage.dvds.lastWrote);
+			var days = Math.round(Math.abs((now.getTime() - lastWrote.getTime())/(_oneDay)));
+			return (days > 1);
+		}
+	}
 }])
 
 /**
