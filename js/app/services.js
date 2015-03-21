8daeaeef1a4d044d7ceadef06af1e65071ca0d0b
Andrew Clinton <ddrew.clinton@gmail.com>
2015-03-21 14:53:07 -0400

    added clean form service

--- js/app/services.js
+++ js/app/services.js
@@ -1,10 +1,10 @@
 angular.module('app.services', [])
 
 /**
-*	This is a custom http service to fill the needs of the two apis
-*	used in this project. If we were using more RESTful API's
-* 	we would have gone with angular's $resource instead.
-*/
+ *  This is a custom http service to fill the needs of the two apis
+ *  used in this project. If we were using more RESTful API's
+ *  we would have gone with angular's $resource instead.
+ */
 .service('HttpService', ['$http', '$q', function($http, $q) {
     return {
         get: function(url) {
@@ -24,16 +24,32 @@ angular.module('app.services', [])
     }
 }])
 
+.service('CleanFormService', [function() {
+    return {
+        clean: function(form) {
+            form.title = '',
+            form.actor = '',
+            form.genre = '',
+            form.year = '',
+            form.minCriticRating = 20,
+            form.minUserRating = 20,
+            form.maxRunTime = 120,
+            form.releasedOnDvd = true,
+            form.releasedInTheater = true
+        }
+    }
+}])
+
 /**
-*	Use this to display console logs only in dev mode to prevent
-* 	outputing sensitive data to the user console in production.
-*/
-.service('Console', ['Environment', function (E) {
-	return {
-		log: function (msg) {
-			if(E.debug){
-				console.log(msg);
-			}
-		}
-	}
+ *  Use this to display console logs only in dev mode to prevent
+ *  outputing sensitive data to the user console in production.
+ */
+.service('Console', ['Environment', function(E) {
+    return {
+        log: function(msg) {
+            if (E.debug) {
+                console.log(msg);
+            }
+        }
+    }
 }]);
