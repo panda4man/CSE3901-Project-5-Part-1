034a9b37d616f4be08db11fb82a2a60896db95a2
Andrew Clinton <ddrew.clinton@gmail.com>
2015-03-21 14:54:30 -0400

    attached form to angular

--- js/app/controllers.js
+++ js/app/controllers.js
@@ -1,11 +1,40 @@
 angular.module('app.controllers', [])
 
-.controller('ApplicationCtrl', function($scope, Environment, $http) {
+.controller('ApplicationCtrl', function($scope, Environment, $http, OpenMovieDatabaseFactory, Console) {
     $scope.debug = Environment.debug;
 })
 
-.controller('MainCtrl', function($scope, $http, OpenMovieDatabaseFactory, Environment, Console) {
-    $scope.movies = $localStorage.movies;
+.controller('MainCtrl', function($scope, OpenMovieDatabaseFactory, Environment, Console, CleanFormService) {
+    $scope.data = {
+        submitted: false,
+        success: false
+    }
+
+    $scope.form = {
+        title:'',
+        actor:'',
+        genre:'',
+        year:'',
+        minCriticRating: 20,
+        minUserRating: 20,
+        maxRunTime: 120,
+        releasedOnDvd: true,
+        releasedInTheater: true
+    }
+    
+    $scope.processForm = function () {
+        $scope.data.submitted = true;
+        OpenMovieDatabaseFactory.search({title: $scope.form.title, year:$scope.form.year}).then(function (data) {
+            $scope.data.submitted = false;
+            $scope.data.success = true;
+            CleanFormService.clean($scope.form);
+            console.log(data);
+        }, function (data) {
+            $scope.data.submitted = false;
+            $scope.data.success = true;
+            console.log(data);
+        })
+    }  
 })
 
 .controller('LandingCtrl', function ($scope){
