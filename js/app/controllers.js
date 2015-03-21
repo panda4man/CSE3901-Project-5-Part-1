12144b3221dcc6272cee836fb0fb153f9aba79cf
Andrew Clinton <ddrew.clinton@gmail.com>
2015-03-21 11:42:34 -0400

    test data

--- js/app/controllers.js
+++ js/app/controllers.js
@@ -1,7 +1,12 @@
 angular.module('app.controllers', [])
 
-.controller('ApplicationCtrl', function($scope, Environment) {
+.controller('ApplicationCtrl', function($scope, Environment, $http) {
     $scope.debug = Environment.debug;
+    $http.get('http://www.omdbapi.com/?t=frozen&y=&plot=full&r=json&tomatoes=true').success(function (data){
+        console.log(data);
+    }).error(function (data){
+        console.log(data);
+    });
 })
 
 .controller('MainCtrl', function($scope, $http, RottenTomatoesFactory, Environment, Console, $localStorage) {
